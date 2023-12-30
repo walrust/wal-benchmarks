56f5691631352d830e18fb601787cbbe316bb5f6
Marcin <marcin.latoszek06@gmail.com>
2023-12-30 14:35:21 +0100

    simple diagrams

--- generate-diagrams.py
+++ generate-diagrams.py
@@ -11,31 +11,47 @@ def average(file_path):
         return np.mean(numbers)
     else:
         raise Exception('No numbers found in ' + file_path)
+    
+def diagram_title(file_path):
+    switch = {
+        'changing-route': 'Zmiana adresu url',
+        'for-adding-to-the-end': 'Dodawanie elementu na koniec intrukcji iteracyjnej',
+        'for-adding-to-the-front': 'Dodawanie elementu na początek intrukcji iteracyjnej',
+        'replacing-whole-100-components-deep-subtree': 'Zamiana całego poddrzewa VDOM o głębokości 100',
+        'sending-callback-through-100-components-up': 'Przesyłanie callbacku przez 100 komponentów w górę drzewa VDOM',
+        'sending-props-100-components-down': 'Przesyłanie Properties przez 100 komponentów w dół drzewa VDOM',
+        'updating-100-children': 'Uaktualnienie 100 komponentów dzieci',
+        'updating-attributes-of-100-children': 'Uaktualnienie atrybutów 100 komponentów dzieci',
+        'updating-self-and-sending-the-same-props-to-100-children': 'Uaktualnienie komponentu i przesłanie tych samych Properties do 100 komponentów dzieci',
+    }
+    return switch.get(os.path.basename(file_path), 'Default')
 
 def generate_diagrams(directory):
     """Recursively process all txt files and create diagrams."""
     for root, dirs, files in os.walk(directory):
-        file_names = []
+        tools = []
         averages = []
         for file in files:
             if file.endswith('.txt'):
                 file_path = os.path.join(root, file)
                 try:
                     avg = average(file_path)
-                    file_names.append(file)
+                    tools.append(file.replace('.txt', ''))
                     averages.append(avg)
                 except Exception as e:
                     print(e)
 
-        if file_names and averages:
+        if tools and averages:
             plt.figure(figsize=(10, 5))
-            plt.bar(file_names, averages, color='blue')
-            plt.xlabel('Files')
-            plt.ylabel('Average Value')
-            plt.title('Bar Chart for Directory: ' + os.path.basename(root))
+            plt.bar(tools, averages, color='blue')
+            plt.xlabel('Narzędzie')
+            plt.ylabel('Średni czas wykonania w mikrosekundach')
+            plt.title(diagram_title(root))
             plt.xticks(rotation=45)
             plt.tight_layout()
-            plt.savefig(os.path.join(root, os.path.basename(root) + '_diagram.png'))
+            diagram_file_name = os.path.basename(root).replace('txt', 'png')
+            diagram_file_path = './diagrams'
+            plt.savefig(os.path.join(diagram_file_path, diagram_file_name))
             plt.close()
 
 generate_diagrams('./results')
